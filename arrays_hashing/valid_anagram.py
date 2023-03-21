a9eaeb721a1e0ddc2e243f5273deda583b9f083f
Kelvin Zhao <yunxi.kelvin@gmail.com>
2023-03-21 13:02:22 -0700

    Complete valid_anagram.py

---
+++ arrays_hashing/valid_anagram.py
@@ -0,0 +1,60 @@
+# own solution
+# Time: O(n log(n))
+# Memory: O(n)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s:list = list(s)
+        t:list = list(t)
+        s.sort()
+        t.sort()
+        if s == t:
+            return True
+        return False
+    
+# Neet basic solution
+# Time: O(s + t)
+# Memory: O(s + t)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+            
+        return True
+
+# Alternate solution using sorting
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+    
+# Most time and memory efficient solution
+# Time: O(n)
+# Memory: O(1)
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        
+        freq = [0] * 26
+        for i in range(len(s)):
+            freq[ord(s[i]) - ord('a')] += 1
+            freq[ord(t[i]) - ord('a')] -= 1
+        
+        for i in range(len(freq)):
+            if freq[i] != 0:
+                return False
+        
+        return True
